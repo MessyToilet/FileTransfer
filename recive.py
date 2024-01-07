be2120d2699f8b19a01642d56f57c6af5d54ae76
Jacob <jacob.chiu168@hotmail.com>
2024-01-06 20:33:41 -0500

    done

---
+++ recive.py
@@ -0,0 +1,37 @@
+import socket, tqdm
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('localhost', 9999))
+server.listen()
+
+client, addr = server.accept() 
+
+
+file_name = client.recv(1024).decode()
+file_size = client.recv(1024).decode()
+
+print(file_name)
+print(file_size)
+
+file = open(file_name, 'wb')
+
+file_bytes = b''
+
+done = False 
+
+progress = tqdm.tqdm(unit="B", unit_scale=True, unit_divisor=1000, total=int(file_size))
+
+while not done:
+    data = client.recv(1024)
+    if file_bytes[-5:] == b'<END>': 
+        done = True
+    else:
+        file_bytes += data 
+
+    progress.update(1024)
+
+file.write(file_bytes)
+client.close()
+server.close()
+
+
